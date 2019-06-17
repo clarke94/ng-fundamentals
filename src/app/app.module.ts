83435ac76e81fb11b7e8510d1b29e62ca114969e
Liam Clarke <liam.clarke.94@outlook.com>
2019-06-17 14:45:58 +0100

    Add Creating Reusable Angular Services section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EventService } from './services/event.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventsListComponent,
-    EventThumbnailComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [EventService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
